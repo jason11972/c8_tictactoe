6696f7ea50729ec8c62e315971df127b96e7c49a
Jason Wilson <jason11972@yahoo.com>
2016-05-03 13:18:43 -0700

    c8_tictactoe - Jason Wilson

---
+++ tictactoe.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Jason Wilson on 5/3/2016.
+ */
+
+
+$(document).ready(function() {
+
+    $(".chooseX").click(function () {
+        console.log("User chooses X");
+       
+    });
+    $(".chooseO").click(function () {
+        console.log("User chooses O");
+
+    });
+}
